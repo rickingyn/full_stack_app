f73bba407721bf2c8cbd846d361517eccf873f3c
rickingyn <rickingyn@gmail.com>
2020-05-12 15:50:05 -0400

    Add button to error pages to return home

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Forbidden = () => {
     return(
         <div className='bounds'>
             <h1>Forbidden</h1>
             <p>Oh oh! You can't access this page.</p>
+            <Link className='button' to='/'>Return to Home Page</Link>
         </div>
     );
 }
--- client/src/components/UnhandledError.js
+++ client/src/components/UnhandledError.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UnhandledError = () => {
     return(
         <div className='bounds'>
             <h1>Error</h1>
             <p>Sorry! We just encountered an unexpected error.</p>
+            <Link className='button' to='/'>Return to Home Page</Link>
         </div>
     );
 }
